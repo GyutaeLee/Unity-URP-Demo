93c0388614c107f6bcd923c8383df44adab87cb8
Gyutae Lee <gyutae07@gmail.com>
2022-02-27 16:12:16 +0900

    feat : EnumMapper.cs 추가 - string 비교로 Enum을 불러오는 기능 담당

---
+++ URP-Demo/Assets/Game/Script/Util/EnumMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Demo.Util
+{
+    public static class EnumMapper
+    {
+        public static T GetEnumType<T>(string name) where T : Enum
+        {
+            Array enumNames = Enum.GetValues(typeof(T));
+            foreach (var enumName in enumNames)
+            {
+                if (enumName.ToString().ToLower() == name.ToLower())
+                {
+                    return (T)enumName;
+                }
+            }
+            Debug.LogError("Error : Invalid enum name");
+            return default;
+        }
+    }
+}
