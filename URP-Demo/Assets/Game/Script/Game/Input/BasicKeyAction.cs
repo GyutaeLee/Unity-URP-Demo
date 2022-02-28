a639e1125da8e24a3034943c1722f8c3da6d8318
Gyutae Lee <gyutae07@gmail.com>
2022-02-28 21:46:56 +0900

    feat : Input 관련 클래스 추가 - IActionProvider : Action 인터페이스 - BasicKeyAction : key 입력 상태 체크 클래스 - TwoAxisAction : 2개의 axis(수직/수평 방향 등) 상태 체크 클래스

---
+++ URP-Demo/Assets/Game/Script/Game/Input/BasicKeyAction.cs
@@ -0,0 +1,40 @@
+﻿namespace Demo.Game.Input
+{
+    public sealed class BasicKeyAction : IActionProvider
+    {
+        public bool IsPressed { get; private set; }
+        public bool WasPressed { get; private set; }
+        public bool WasReleased { get; private set; }
+
+        private string key;
+
+        public BasicKeyAction(string key)
+        {
+            this.key = key;
+        }
+
+        public void Run()
+        {
+            bool isStay = UnityEngine.Input.GetButton(key);
+            bool isEnter = UnityEngine.Input.GetButtonDown(key);
+            bool isRelease = UnityEngine.Input.GetButtonUp(key);
+
+            if (isEnter == true && this.IsPressed == false)
+            {
+                this.IsPressed = true;
+                this.WasReleased = false;
+            }
+            else if (this.IsPressed == true && isStay == true)
+            {
+                this.WasPressed = true;
+                this.IsPressed = false;
+            }
+
+            if (isRelease == true)
+            {
+                this.WasPressed = false;
+                this.WasReleased = true;
+            }
+        }
+    }
+}
