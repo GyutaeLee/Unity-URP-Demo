06e29bf806ad4d07d6841ee51833fd2a260b6f09
Gyutae Lee <gyutae07@gmail.com>
2022-03-15 22:12:25 +0900

    fix : BasicKeyAction.cs 수정 - IsStay : public 변수로 수정 - wasRelease 로직 수정 : 바로 이전 프레임에서 뗀 경우에만 true

--- URP-Demo/Assets/Game/Script/Game/Input/BasicKeyAction.cs
+++ URP-Demo/Assets/Game/Script/Game/Input/BasicKeyAction.cs
@@ -2,6 +2,7 @@
 {
     public sealed class BasicKeyAction : IActionProvider
     {
+        public bool IsStay { get; private set; }
         public bool IsPressed { get; private set; }
         public bool WasPressed { get; private set; }
         public bool WasReleased { get; private set; }
@@ -15,7 +16,7 @@
 
         public void Run()
         {
-            bool isStay = UnityEngine.Input.GetButton(key);
+            this.IsStay = UnityEngine.Input.GetButton(key);
             bool isEnter = UnityEngine.Input.GetButtonDown(key);
             bool isRelease = UnityEngine.Input.GetButtonUp(key);
 
@@ -24,16 +25,20 @@
                 this.IsPressed = true;
                 this.WasReleased = false;
             }
-            else if (this.IsPressed == true && isStay == true)
+            else if (this.IsPressed == true && this.IsStay == true)
             {
                 this.WasPressed = true;
                 this.IsPressed = false;
             }
 
-            if (isRelease == true)
+            if (this.WasReleased == false && isRelease == true)
             {
                 this.WasPressed = false;
                 this.WasReleased = true;
+            }
+            else if (this.WasReleased == true && isRelease == false)
+            {
+                this.WasReleased = false;
             }
         }
     }
